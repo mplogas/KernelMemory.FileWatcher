25afc8af1a3b282ba490343e1829710285515ebb
Marc Plogas <marc.plogas@digitalnatives.de>
2024-03-15 20:57:31 +0100

    filewatcher now accepts a list of filters

--- KernelMemory.FileWatcher/Configuration/FileWatcherDirectoryOptions.cs
+++ KernelMemory.FileWatcher/Configuration/FileWatcherDirectoryOptions.cs
@@ -3,7 +3,8 @@
     internal class FileWatcherDirectoryOptions
     {
         public string Path { get; set; }
-        public string Filter { get; set; }
+        public string Filter { get; set; } = string.Empty;
+        public List<string> Filters { get; set; } = new();
         public bool IncludeSubdirectories { get; set; }
         public string Index { get; set; }
         public bool InitialScan { get; set; }
--- KernelMemory.FileWatcher/Services/FileWatcherFactory.cs
+++ KernelMemory.FileWatcher/Services/FileWatcherFactory.cs
@@ -3,11 +3,30 @@
     internal interface IFileWatcherFactory
     {
         public FileSystemWatcher Create(string directory, string filter, bool recursive);
+        public FileSystemWatcher Create(string directory, List<string> filters, bool recursive);
     }
 
     internal class FileWatcherFactory : IFileWatcherFactory
     {
         public FileSystemWatcher Create(string directory, string filter, bool recursive = true)
+        {
+            var watcher = BuildFileSystemWatcher(directory, recursive);
+            watcher.Filter = filter;
+
+            return watcher;
+        }
+
+        public FileSystemWatcher Create(string directory, List<string> filters, bool recursive)
+        {
+            var watcher = BuildFileSystemWatcher(directory, recursive);
+            foreach (var filter in filters)
+            {
+                watcher.Filters.Add(filter);
+            }
+            return watcher;
+        }
+
+        private FileSystemWatcher BuildFileSystemWatcher(string directory, bool recursive = true)
         {
             //BUG: a deleted subfolder is not catched by FileSystemWatcher (including all containing files)
             var watcher = new FileSystemWatcher(directory);
@@ -17,9 +36,6 @@
                                    | NotifyFilters.FileName
                                    | NotifyFilters.LastWrite
                                    | NotifyFilters.Size;
-
-            watcher.Filter = filter;
-            watcher.IncludeSubdirectories = recursive;
             watcher.EnableRaisingEvents = true;
 
             return watcher;
